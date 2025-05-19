02408cc892f64c7469da1b512ab2b3fb8ce0728e
Kunal Singh <kunalsingh14042003@gmail.com>
2025-05-19 18:49:12 +0530

    gunicorn

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def add_numbers():
         return jsonify({'error': 'Please provide valid numbers'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
